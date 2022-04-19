536679c84f6a93299e935f7595ac809d45c78deb
Barış Ertakuş <bariserta@gmail.com>
2022-04-20 02:08:17 +0300

    Add Custom Input

---
+++ src/components/core/Input.js
@@ -0,0 +1,6 @@
+import { Input } from "native-base";
+import { hp } from "../../utils/responsiveScreen";
+
+export default () => {
+  return <Input borderRadius={10} height="12" backgroundColor="#F8F8FA" />
+}
