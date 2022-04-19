3b3e24bee828431a9be8eb1c33df5e66ed4897b4
Barış Ertakuş <bariserta@gmail.com>
2022-04-20 02:07:31 +0300

    Add Layout & SafeLayout

---
+++ src/components/core/SafeLayout.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+export default styled(SafeAreaView)`
+  flex: 1;
+  background-color: white;
+`
