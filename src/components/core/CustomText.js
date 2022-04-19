13530d805d3d41a63a8dc1eb2f5987d76eb91ca8
Barış Ertakuş <bariserta@gmail.com>
2022-04-19 22:43:51 +0300

    Add CustomText
    A reusable CustomText component has been created instead of using react native's Text component.

---
+++ src/components/core/CustomText.js
@@ -0,0 +1,39 @@
+import { Text } from "react-native";
+import React from "react";
+import rf from "../../utils/responsiveFont";
+
+const CustomText = ({
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  p,
+  bold,
+  italic,
+  title,
+  style,
+  ...rest
+}) => {
+  return (
+    <Text
+      style={[
+        h1 && { fontSize: rf(48) },
+        h2 && { fontSize: rf(32) },
+        h3 && { fontSize: rf(20) },
+        h4 && { fontSize: rf(18) },
+        h5 && { fontSize: rf(16) },
+        h5 && { fontSize: rf(16) },
+        p && { fontSize: rf(12) },
+        bold && { fontWeight: "bold" },
+        italic && { fontStyle: "italic" },
+        style,
+      ]}
+      {...rest}
+    >
+      {title}
+    </Text>
+  );
+};
+
+export default CustomText;
