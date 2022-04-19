658a69f690ceb086d734a4023227ca840e5ca7e5
Barış Ertakuş <bariserta@gmail.com>
2022-04-19 23:54:43 +0300

    Create Appointment structure

---
+++ src/components/appointment/DayCards.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DayCards = () => {
+  return (
+    <View>
+      <Text>DayCards</Text>
+    </View>
+  )
+}
+
+export default DayCards
+
+const styles = StyleSheet.create({})
--- src/screens/Appointment.js
+++ src/screens/Appointment.js
@@ -1,22 +1,49 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { Text, TouchableOpacity, View } from "react-native";
+import React from "react";
 import styled from "styled-components";
-import { SafeAreaView } from 'react-native-safe-area-context';
-import CustomText from '../components/core/CustomText';
+import { SafeAreaView } from "react-native-safe-area-context";
+import CustomText from "../components/core/CustomText";
 import { wp } from "../utils/responsiveScreen";
-import Header from '../components/core/Header';
+import Header from "../components/core/Header";
+import DayCards from "../components/appointment/DayCards";
+import TimeCards from "../components/appointment/TimeCards";
+import { Input } from "react-native-elements";
 
 const Appointment = () => {
   return (
     <SafeAreaView>
       <Header header="New Appointment" />
+      <Container>
+        <Days>
+          <CustomText h5 bold title="April, 2022" />
+          <DayCards />
+        </Days>
+        <Times>
+          <CustomText h5 bold title="Available Time" />
+          <TimeCards />
+        </Times>
+        <Details>
+        <CustomText p title="Full name" />
+        <Input />
+        <CustomText p title="Duration" />
+        <Input />
+        <CustomText p title="Description" />
+        <Input />
+        <TouchableOpacity><CustomText title="Set Appointment" /></TouchableOpacity>
+        </Details>
+      </Container>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Appointment
+export default Appointment;
 
-const Wrapper = styled.View`
-  width: ${wp("50%")};
-  border: 1px solid black;
-`
+const Container = styled.View`
+  margin: 0 ${wp("5%")}px;
+`;
+
+const Days = styled.View``;
+
+const Times = styled.View``;
+
+const Details = styled.View``;
